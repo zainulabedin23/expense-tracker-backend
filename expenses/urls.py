b355bebb77faade5e4e17724254071987011d212
zainulabedin23 <abedinz427@gmail.com>
2025-02-21 00:12:53 +0530

    added simplified debt api

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ExpenseViewSet, ExpenseSplitViewSet
 from .views import PendingExpensesView
-from .views import UserExpensesViewSet
+from .views import UserExpensesViewSet,SimplifyDebtView
 router = DefaultRouter()
 router.register(r'expenses', ExpenseViewSet)
 router.register(r'expense-splits', ExpenseSplitViewSet)
@@ -11,4 +11,5 @@ urlpatterns = [
     path('expense/', include(router.urls)),
     path('pending-expenses/<uuid:user_id>/', PendingExpensesView.as_view(), name='pending-expenses'),
     path('user-expenses/<uuid:user_id>/', UserExpensesViewSet.as_view({'get': 'list'}), name='user-expenses'),
+    path('simplify-debts/<uuid:group_id>/', SimplifyDebtView.as_view(), name='simplify-debts'),
 ]
